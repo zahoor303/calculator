6c133e943cd2013f9fb4672a6a0a43378c3953bb
ZAHOOR FATIMA <160278589+zahoor303@users.noreply.github.com>
2024-03-24 22:00:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import inquirer from "inquirer";
+const asnwer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+console.log(asnwer);
